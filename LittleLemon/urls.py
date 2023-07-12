f90b9ae518816c41648f43e7c9c69b19234b6145
Fei Deng <fei.deng@fisherpaykel.com>
2023-07-12 21:11:34 +1200

    Default restaurant to home page

--- LittleLemon/urls.py
+++ LittleLemon/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('restaurant/', include(('restaurant.urls', 'restaurant'), namespace='restaurant')),
+    path('', include(('restaurant.urls', 'restaurant'), namespace='restaurant')),
 ]
