da73485361ca214fd28b3362409907a75dcf1ca1
Fei Deng <fei.deng@fisherpaykel.com>
2023-07-12 20:32:58 +1200

    Update start script comment

--- start.ps1
+++ start.ps1
@@ -24,14 +24,14 @@ $counter = 0
 
 # Keep testing the connection until it is true or more than 10 times
 while ($connection -eq $false -and $counter -lt 10 ) {
-    # connection equals to result of Test-NetConnection TcpTestSucceeded
     $test = Test-NetConnection $DB_SERVER -Port $DB_PORT
     $connection = $test.TcpTestSucceeded
     Write-Host "Waiting for the backend db to start..."
     Start-Sleep -s 3
 }
 
-# Start the web server by running 'python manage.py runserver' if the connection is true
+# Start the web server by running 'python manage.py runserver'
+# if the connection is true
 if ($connection -eq $true) {
     Write-Host "Starting the web server..."
     python manage.py runserver
@@ -40,12 +40,5 @@ else {
     Write-Host "Failed to start the database server..."
     Write-Host "Failed to start the web server..."
 }
-
-
-
-
-
-
-
 
 
