f5850b515368aa57b2e2b8aa609d211928abbcc3
Fei Deng <fei.deng@fisherpaykel.com>
2023-07-17 10:31:41 +1200

    Move tests folder into project level

--- LittleLemon/tests/test_models.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from restaurant.models import Menu
-
-# Create your tests here.
-class MenuItemTest(TestCase):
-    def test_get_item(self):
-        item = Menu.objects.create(title="Cheese Burger", price=5.99, inventory=10)
-        self.assertEqual(str(item), "Cheese Burger : $5.99")
